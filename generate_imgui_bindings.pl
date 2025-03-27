f6a61fd512a257a5ae03373d77a9ee58cacffbc2
Jim Keir <jimkeir@users.noreply.github.com>
2025-03-27 12:36:35 +0000

    Fix optional ImVec2/4 arguments, 263 supported functions

--- generate_imgui_bindings.pl
+++ generate_imgui_bindings.pl
@@ -254,9 +254,9 @@ sub generateImguiGeneric {
           }
           push(@funcArgs, $name);
         #const ImVec2& with default or not
-        } elsif ($args[$i] =~ m/^ *const ImVec2& ([^ ]*) *(= * ImVec2 [^ ]* [^ ]*|) *$/) {
+        } elsif ($args[$i] =~ m/^ *const ImVec2& ([^ ]*) *(= * ImVec2 [^\s]+ +[^\s]+|) *$/) {
           my $name = $1;
-          if ($2 =~ m/^= * ImVec2 ([^ ]*) ([^ ]*)$/) {
+          if ($2 =~ m/^= * ImVec2 ([^\s]+)\s+([^\s]+)$/) {
             push(@before, "OPTIONAL_IM_VEC_2_ARG($name, $1, $2)");
           } else {
             push(@before, "IM_VEC_2_ARG($name)");
@@ -268,9 +268,9 @@ sub generateImguiGeneric {
           push(@before, "IM_VEC_2_ARG($name)");
           push(@funcArgs, $name);
         #const ImVec4& with default or not
-        } elsif ($args[$i] =~ m/^ *const ImVec4& ([^ ]*) *(= * ImVec4 [^ ]* [^ ]* [^ ]* [^ ]*|) *$/) {
+        } elsif ($args[$i] =~ m/^ *const ImVec4& ([^ ]*) *(= * ImVec4 [^\s]*\s+[^\s]*\s+[^\s]*\s+[^\s]*|) *$/) {
           my $name = $1;
-          if ($2 =~ m/^= * ImVec4 ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*)$/) {
+          if ($2 =~ m/^= * ImVec4 ([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)$/) {
             push(@before, "OPTIONAL_IM_VEC_4_ARG($name, $1, $2, $3, $4)");
           } else {
             push(@before, "IM_VEC_4_ARG($name)");
